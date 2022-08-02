77a4c9f35cd53d18b9171d70a041a97ac3bebc58
Emre <79599681+emre9180@users.noreply.github.com>
2022-08-02 03:46:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ queue = []
 
 @app.route('/', methods=['GET'])
 def index():
-  res =  {"sunucuip": "http://192.168.46.179:5000","rtsp": "rtsp://192.168.1.2:554/1/1"
+  res =  {"sunucuip": "http://192.168.46.179:5000","rtsp": "rtsp://192.168.1.41:554/1/1"
 
 if __name__ == '__main__':
     app.run()
